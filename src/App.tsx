b3b3fffce9653d5237ff5f89db871ee85f781d2e
Conner <conner.tran@bsa.edu.vn>
2025-09-14 21:57:10 +0200

    was working on react routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import TodoPage from "./pages/TodoPage";
 import FormPage from "./pages/FormPage";
 import FetchApiPage from "./pages/FetchApiPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      {/* <TodoPage />
-      <FormPage /> */}
-      <FetchApiPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TodoPage />} />
+          <Route path="/form-page" element={<FormPage />} />
+          <Route path="/fetch-api/:name" element={<FetchApiPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/FetchApiPage.tsx
+++ src/pages/FetchApiPage.tsx
@@ -1,13 +1,15 @@
 import userEvent from "@testing-library/user-event";
 import useToggle from "../customHooks/toggleHook";
 import "./FetchApiPage.css";
+import { useParams } from "react-router-dom";
 function FetchApiPage() {
   const [theme, setTheme] = useToggle(true);
   const [mood, setMood] = useToggle(true);
-
+  const { name } = useParams<{ name: string }>();
   return (
     <div className={theme ? "light-mode" : "dark-mode"}>
       <h1>{mood ? "😂" : "😭"}</h1>
+      <h2>{name}</h2>
       <button
         onClick={() => {
           setMood();
